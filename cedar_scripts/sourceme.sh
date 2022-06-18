db0ace7d6d77e7d7f47943dd3955454510b59032
RonArnab <82034695+Arnab-Sarker@users.noreply.github.com>
2022-06-18 15:55:07 +0530

    Update sourceme.sh
    changing the source locations of ROOT/geant4/wcsim to my newly installed folder

--- cedar_scripts/sourceme.sh
+++ cedar_scripts/sourceme.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 module load StdEnv/2016
-module load gcc/9.1.0
+module load gcc/6.4.0
 module load python/3.6.3
 module load scipy-stack
-source /project/rpp-blairt2k/machine_learning/production_software/root/install/bin/thisroot.sh
-source /project/rpp-blairt2k/machine_learning/production_software/Geant4/geant4.10.01.p03-install/share/Geant4-10.1.3/geant4make/geant4make.sh
-export G4WORKDIR=/project/rpp-blairt2k/machine_learning/production_software/WCSim/exe
+
+source /home/arnabs/scratch/root/root/install/bin/thisroot.sh
+source /home/arnabs/scratch/geant4/geant4.10.03.p03/install/share/Geant4-10.3.3/geant4make/geant4make.sh
+
+export G4WORKDIR=/home/arnabs/scratch/WCTE1/WCTE_WCSim/exe
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}${G4LIB}/${G4SYSTEM}
-export WCSIMDIR=/project/rpp-blairt2k/machine_learning/production_software/WCSim
+export WCSIMDIR=/home/arnabs/scratch/WCTE1/WCTE_WCSim
 export DATATOOLS="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=$DATATOOLS:$PYTHONPATH
