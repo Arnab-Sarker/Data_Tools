804fa041e607bc8baccfcb43fcbb25f53e9fbd87
RonArnab <82034695+Arnab-Sarker@users.noreply.github.com>
2022-06-21 10:37:38 +0530

    Update sourceme.sh
    Updating the locations of ROOT and geant4

--- cedar_scripts/sourceme.sh
+++ cedar_scripts/sourceme.sh
@@ -4,11 +4,11 @@ module load gcc/6.4.0
 module load python/3.6.3
 module load scipy-stack
 
-source /home/arnabs/scratch/root/root/install/bin/thisroot.sh
-source /home/arnabs/scratch/geant4/geant4.10.03.p03/install/share/Geant4-10.3.3/geant4make/geant4make.sh
+source /home/arnabs/scratch/Root/root/install/bin/thisroot.sh
+source /home/arnabs/scratch/Geant4/geant4.10.01.p03/install/share/Geant4-10.1.3/geant4make/geant4make.sh
 
-export G4WORKDIR=/home/arnabs/scratch/WCTE1/WCTE_WCSim/exe
+export G4WORKDIR=/home/arnabs/scratch/WCTE_wcsim/WCTE_WCSim/exe
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}${G4LIB}/${G4SYSTEM}
-export WCSIMDIR=/home/arnabs/scratch/WCTE1/WCTE_WCSim
+export WCSIMDIR=/home/arnabs/scratch/WCTE_wcsim/WCTE_WCSim
 export DATATOOLS="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=$DATATOOLS:$PYTHONPATH
